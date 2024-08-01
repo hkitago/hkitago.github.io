ddd4a1a19edd2f75d602bf443f9375193730c210
hkitago <hkitago@gmail.com>
2024-08-01 14:51:59 +0900

    Update glitchtextcss.js
    Added myApp object to store app and settings.

--- glitchtextcssjs/glitchtextcss.js
+++ glitchtextcssjs/glitchtextcss.js
@@ -1,24 +1,31 @@
-(()=>{
-  'use strict'
-  const app = {}, settings = {}
-  app.d = document
-  app.glitches = app.d.getElementsByClassName('glitch')
-  settings.fps = 21
-  settings.minRect = 0
-  
-  for(let i = 0, max = app.glitches.length; i < max; i++){
-    const rect = app.glitches[i].getBoundingClientRect()
-    let cssText = '@keyframes glitch {' + "\n"
-    for(let j = 0, max = settings.fps; j < max; j++){
-      const rectTop = Math.floor(Math.random() * (rect.height - settings.minRect)) + settings.minRect
-      const rectBottom = Math.floor(Math.random() * (rect.height - rectTop)) + rectTop
-      cssText += (j / settings.fps) * 100 + '% {clip: rect(' + rectTop + 'px, auto, '+rectBottom+'px, 0);}' + "\n"
-    }
-    cssText += '}'
-    const sE = app.d.createElement('style')
-    ,     tE = app.d.createTextNode(cssText)
-    sE.appendChild(tE)
-    app.d.head.appendChild(sE)
-    app.glitches[i].classList.add('on')
-  }
-})()
+(() => {
+  'use strict';
+
+  const myApp = {
+    app: {},
+    settings: {}
+  };
+
+  myApp.app.d = document;
+  myApp.app.glitches = Array.from(myApp.app.d.getElementsByClassName('glitch'));
+  myApp.settings.fps = 21;
+  myApp.settings.minRect = 0;
+
+  myApp.app.glitches.forEach((glitch) => {
+    const rect = glitch.getBoundingClientRect();
+    let cssText = '@keyframes glitch {\n';
+
+    Array.from({ length: myApp.settings.fps }).forEach((_, j) => {
+      const rectTop = Math.floor(Math.random() * (rect.height - myApp.settings.minRect)) + myApp.settings.minRect;
+      const rectBottom = Math.floor(Math.random() * (rect.height - rectTop)) + rectTop;
+      cssText += (j / myApp.settings.fps) * 100 + '% {clip: rect(' + rectTop + 'px, auto, ' + rectBottom + 'px, 0);}\n';
+    });
+
+    cssText += '}';
+    const sE = myApp.app.d.createElement('style');
+    const tE = myApp.app.d.createTextNode(cssText);
+    sE.appendChild(tE);
+    myApp.app.d.head.appendChild(sE);
+    glitch.classList.add('on');
+  });
+})();
